752503e8390a606b7c558d5ae988014f97980339
Mark <duffy_mark@hotmail.com>
2021-12-16 10:21:08 -0400

    branch up to date save. worked quirks

--- JavaScript/burger.js
+++ JavaScript/burger.js
@@ -10,37 +10,4 @@
   }
 }
 
-/* ----------- Email Submit ---------------------*/
- const submitted = document.querySelector('.footerButton');
-
-  function submitEmail() {
-                document.getElementById("footerEmail").submitEmail();
-            }
-
- submitted.addEventListener('click', function(){
-  console.log("click it");
-  return submitEmail();
-  });           
-
-
-
-/*
-const submitted = document.querySelector('.footerButton');
-
-function submitEmailAddress(footerEmail) {
-  footerEmail.submit();
-};
-
-submitted.addEventListener('click', function(){
-  console.log("click it");
-  submitEmailAddress();
-  });
-
-function submitEmailAddress() {
-  document.getElementById("#footerEmail").submitEmail();
-}
-*/
-
-
-
 
